6819700a972b064ba4bf36ddfd3cca19d0534392
nivri740 <34022347+nivri740@users.noreply.github.com>
2022-11-12 01:27:48 +0800

    bug fix

--- app/Http/Controllers/volunteer_type_controller.php
+++ app/Http/Controllers/volunteer_type_controller.php
@@ -1,5 +1,5 @@
 <?php
-    
+
 namespace App\Http\Controllers;
 use DB;
 use App\Models\roles;
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\member_profile;
 use App\Models\volunteer_type;
 use App\Http\Controllers\Controller;
-    
+
 class volunteer_type_controller extends Controller
 {
     /**
@@ -22,13 +22,13 @@ class volunteer_type_controller extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index(Request $request)
-    { 
+    {
         return view ('roles.roles',[
             'vroles' => roles::get(),
         ]);
-  
+
     }
-    
+
     /**
      * Show the form for creating a new resource.
      *
@@ -39,7 +39,7 @@ class volunteer_type_controller extends Controller
         $alltypes = roles::get();
         return view('volunteertype.assigntype',compact('alltypes'));
     }
-    
+
     /**
      * Store a newly created resource in storage.
      *
@@ -48,10 +48,10 @@ class volunteer_type_controller extends Controller
      */
     public function store(Request $request)
     {
-      
-        
+
+
         foreach ($request->types as $key=>$name){
-           
+
             $insert = [
                 'profileId' =>json_decode($request ->profileId)->profileId,
                 'roles' => $request -> types[$key]
@@ -59,39 +59,39 @@ class volunteer_type_controller extends Controller
             DB::table('volunteer_type')->insert($insert);
         }
         $profiles = member_profile::orderBy('profileId','ASC')->paginate(5);
-        
+
         return view('profile.profiles',compact('profiles'))
             ->with('i', ($request->input('page', 1) - 1) * 5);
-    
+
 
     }
     public function show($id)
     {
         return redirect()->route('profile.show',$id);
     }
-    
-    
+
+
     /**
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)   
+    public function edit($id)
     {
 
         $profileId=$id;
-        
+
         $volunteer_type = DB::table("volunteer_type")->where("volunteer_type.profileId",$id)
             ->pluck('volunteer_type.roles','volunteer_type.roles')
             ->all();
-        
-        
+
+
         $alltypes = roles::all();
-       
+
         return view('volunteertype.edittype',compact('profileId','alltypes','volunteer_type'));
     }
-    
+
     /**
      * Update the specified resource in storage.
      *
@@ -101,52 +101,53 @@ class volunteer_type_controller extends Controller
      */
     public function update(Request $request, $id)
     {
-        
+
         $vprofile = volunteer_type::where('profileId',$id)->get();
-        
+
         if(count($vprofile)==0){
-            
+
             foreach ($request->types as $key=>$name){
                 $insert = [
                     'profileId' =>$id,
-                    'roles' => $request -> types[$key]  
+                    'roles' => $request -> types[$key]
                 ];
-                    
-                DB::table('volunteer_type')->insert($insert); 
-                
+
+                DB::table('volunteer_type')->insert($insert);
+
             }
-            
+
         }
         else{
         foreach($vprofile as $row){
             if($request->types!=null){
+                volunteer_type::where('profileId',$id)->delete();
                 foreach ($request->types as $key=>$name){
                     if(!$row->roles==$request -> types[$key] ){
                         $insert = [
                             'profileId' =>$id,
-                            'roles' => $request -> types[$key]  
+                            'roles' => $request -> types[$key]
                         ];
-                        DB::table('volunteer_type')->insert($insert); 
+                        DB::table('volunteer_type')->insert($insert);
                     }
                     else{
-                        volunteer_type::where('profileId','=',$id)->where('roles',$request -> types[$key])->delete();  
+                        volunteer_type::where('profileId','=',$id)->where('roles',$request -> types[$key])->delete();
                         $insert = [
                             'profileId' =>$id,
-                            'roles' => $request -> types[$key]  
+                            'roles' => $request -> types[$key]
                         ];
-                        DB::table('volunteer_type')->insert($insert); 
+                        DB::table('volunteer_type')->insert($insert);
                     }
-                } 
+                }
             }
             else{
-                volunteer_type::where('profileId','=',$id)->delete();  
+                volunteer_type::where('profileId','=',$id)->delete();
             }
         }
     }
-        
-        
+
+
         return redirect()->route('profile.show',$id);
-                        
+
     }
     /**
      * Remove the specified resource from storage.
